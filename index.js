1aeb07830a8435dd3ed3a0c5b2b289c969c93b8d
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-06-24 16:50:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let greeting = "";
+  if (someValue > 2500) {
+    greeting = "No can do.";
+  }
+  else if (someValue > 2000) {
+    greeting = "I will gladly take your thirty bucks.";
+  }
+  else if (someValue <= 400){
+    greeting = "This one is on me!";
+  }
+
+  return greeting;
+}
+
+function ternaryCheckCity(city) {
+  let status = (city === "NYC")? "Ok, sounds good." : "No go.";
+  return status;
+}
+
+function switchOnCharmFromTip(tip) {
+  let response;
+
+  switch(tip) {
+    case "generous":
+      response = "Thank you so much.";
+      break;
+    case "not as generous":
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye.";
+      break;
+
+  }
+  return response;
+}
